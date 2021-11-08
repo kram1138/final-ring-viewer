aeaf45f0d1eaf6a389a254dd3cef7de3ff868c74
Mark Van Egmond <mark.vanegmond@conquestplanning.com>
2021-11-08 16:42:10 -0600

    Fix promise imports

--- src/routes/points/[region].ts
+++ src/routes/points/[region].ts
@@ -1,11 +1,11 @@
-import { readFile, writeFile } from "fs/promises";
+import fs from "fs";
 
 export async function get({params}) {
-    const points = JSON.parse((await readFile(`points/${params.region}.json`)).toString());
+    const points = JSON.parse((await fs.promises.readFile(`points/${params.region}.json`)).toString());
     return { body: points };
 }
 
 export async function put({body, params}) {
-    await writeFile(`points/${params.region}.json`, body);
+    await fs.promises.writeFile(`points/${params.region}.json`, body);
     return {status: 200};
 }
